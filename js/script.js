ba0ce60731889fe569424c54dda35fe7f75937eb
Itina Winter <lobanova.irina.96@mail.ru>
2020-03-14 18:38:51 +0300

    Завершает реализацию функции всплывающих окон

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 
 var switches__radio = document.querySelectorAll('.switches__radio');
 var slide = document.querySelectorAll('.slide');
-
 var services__btn = document.querySelectorAll('.services__btn');
 var services__description = document.querySelectorAll('.services-description__item');
 
@@ -29,18 +28,26 @@ slider(switches__radio, slide);
 
 var btn_contacts = document.querySelector('.btn-contacts');
 var btn_map = document.querySelector('.map');
+var btn_buy = document.querySelectorAll('.product-action__button-buy');
+var popup_status = document.querySelector('.container-status');
 var popup_contacts = document.querySelector('.container-contacts');
 var popup_map = document.querySelector('.container-map');
 
 function open(button, popup) {
-  button.addEventListener('click', function() {
-    popup.classList.remove('hidden');
-    event.preventDefault();
-  })
+  if (button && popup) {
+    button.addEventListener('click', function() {
+      popup.classList.remove('hidden');
+      event.preventDefault();
+    });
+  } 
 }
 
+btn_buy.forEach(function(e, i) {
+  open(btn_buy[i], popup_status)
+});
 open(btn_contacts, popup_contacts);
 open(btn_map, popup_map);
+
 
 
 // [CLOSE WINDOW]
