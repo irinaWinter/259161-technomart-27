c7a2b09c4463ed41e7cf0b7721d66dd02e4c6054
Itina Winter <lobanova.irina.96@mail.ru>
2020-03-14 15:46:22 +0300

    Добавляет функции открытия и закрытия для всплывающих окон

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ var services__description = document.querySelectorAll('.services-description__it
 function slider(switchers, slides) {
   var current = 0;
   switchers.forEach(function(e, i) {
-    e.addEventListener('click', function () {
+    e.addEventListener('click', function() {
       if (i !== current) {
         slides[current].classList.add('visually-hidden');
         slides[i].classList.remove('visually-hidden');
@@ -23,3 +23,36 @@ function slider(switchers, slides) {
 
 slider(services__btn, services__description);
 slider(switches__radio, slide);
+
+
+// [SHOW WINDOW]
+
+var btn_contacts = document.querySelector('.btn-contacts');
+var btn_map = document.querySelector('.map');
+var popup_contacts = document.querySelector('.container-contacts');
+var popup_map = document.querySelector('.container-map');
+
+function open(button, popup) {
+  button.addEventListener('click', function() {
+    popup.classList.remove('hidden');
+    event.preventDefault();
+  })
+}
+
+open(btn_contacts, popup_contacts);
+open(btn_map, popup_map);
+
+
+// [CLOSE WINDOW]
+
+var btn_close = document.querySelectorAll('.btn-close');
+
+function close(button) {
+  button.forEach(function(e, i) {
+    button[i].addEventListener('click', function() {
+      button[i].parentNode.parentNode.classList.add('hidden');
+    });
+  });
+}
+
+close(btn_close);
