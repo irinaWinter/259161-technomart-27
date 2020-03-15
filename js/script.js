4bc9b667f51b59a601e99727af618075e7e418e7
Itina Winter <lobanova.irina.96@mail.ru>
2020-03-15 15:16:16 +0300

    Реализует возможность добавления товара в корзину или в закладки

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+var btn_buy = document.querySelectorAll('.product-action__button-buy');
+var btn_bookmarks = document.querySelectorAll('.product-action__button-bookmark');
+
 // [SLIDER]
 
 var switches__radio = document.querySelectorAll('.switches__radio');
@@ -20,15 +23,11 @@ function slider(switchers, slides) {
   });
 }
 
-slider(services__btn, services__description);
-slider(switches__radio, slide);
-
 
 // [SHOW WINDOW]
 
 var btn_contacts = document.querySelector('.btn-contacts');
 var btn_map = document.querySelector('.map');
-var btn_buy = document.querySelectorAll('.product-action__button-buy');
 var popup_status = document.querySelector('.container-status');
 var popup_contacts = document.querySelector('.container-contacts');
 var popup_map = document.querySelector('.container-map');
@@ -42,13 +41,6 @@ function open(button, popup) {
   } 
 }
 
-btn_buy.forEach(function(e, i) {
-  open(btn_buy[i], popup_status)
-});
-open(btn_contacts, popup_contacts);
-open(btn_map, popup_map);
-
-
 
 // [CLOSE WINDOW]
 
@@ -61,5 +53,49 @@ function close(button) {
     });
   });
 }
+
+
+// [CLOSE WINDOW]
+function addToBasket() {
+  var basket = document.querySelector('.shopping__link-basket');
+  var basket_value = document.querySelector('.basket-value');
+  var count = Number(basket_value.textContent);
+
+  basket.classList.add('shopping-full');
+  count++;
+  basket_value.textContent = ' ' + count;
+}
+
+function addToBookmarks() {
+  var bookmarks = document.querySelector('.shopping__link-bookmarks');
+  var bookmarks_value = document.querySelector('.bookmarks-value');
+  var count = Number(bookmarks_value.textContent);
+
+  bookmarks.classList.add('shopping-full');
+  count++;
+  bookmarks_value.textContent = ' ' + count;
+  event.preventDefault();  
+}
+
+
+// Вызовы
+
+slider(services__btn, services__description);
+slider(switches__radio, slide);
+
+btn_buy.forEach(function(e, i) {
+  open(btn_buy[i], popup_status);
+});
+
+btn_buy.forEach(function(e, i) {
+  btn_buy[i].addEventListener('click', addToBasket);
+});
+
+btn_bookmarks.forEach(function(e, i) {
+  btn_bookmarks[i].addEventListener('click', addToBookmarks);
+});
+
+open(btn_contacts, popup_contacts);
+open(btn_map, popup_map);
 
 close(btn_close);
